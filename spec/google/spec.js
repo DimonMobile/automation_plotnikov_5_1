f123ee41400553df136509d9572e357ed19d6055
Dimon Mobile <mobik.dimka@gmail.com>
2019-12-14 21:30:38 +0300

    experiments

--- spec/google/spec.js
+++ spec/google/spec.js
@@ -1,16 +1,3 @@
-// arguments processing logic
-let browser = 'chrome';
-if (process.argv.length > 2) {
-    let regex = /--browser=(\w+)/;
-    browser = process.argv[2].match(regex)[1];
-}
-
-// preparing selenium, config and logger
-require('chromedriver');
-require('geckodriver');
-
-const Config = require('../../testData.js');
-
 // logger configuration
 const log4js = require('log4js');
 
@@ -38,8 +25,10 @@ let logger = {
 }
 
 // preparing webdriver
+//let browser = require('wdio').getBrowser('UiAutomator2');
+require('wdio');
 const { Builder, By, Key, until } = require('selenium-webdriver');
-let driver = new Builder().forBrowser(browser).build();
+//let driver = new Builder().forBrowser('UiAutomator2').build();
 let resultsCount = 0;
 let dateStarted;
 
@@ -50,58 +39,21 @@ jasmine.getEnv().addReporter(new AllureReporter({
 }));
 
 // preparing PageObject pattern
-let GooglePage = require('./google-page.js');
 
-let page = new GooglePage.Page(driver, By, Key, until, logger);
-let resultsPage = null;
-
-beforeAll(async function () {
-    logger.trace('beforeAll');
-    dateStarted = new Date();
-    await page.open();
-    await page.setSearchQuery(Config.searchString);
-    resultsPage = await page.submitQuery();
+beforeAll(function () {
+    logger.info('before all');
 }, 20000);
 
-afterAll(async function () {
-    await driver.quit();
-    logger.trace(`Results: ${resultsCount}; Time: ${new Date() - dateStarted}ms`);
-
-    await log4js.shutdown();
+afterAll(function () {
+    logger.info('after all');
+    log4js.shutdown();
 }, 20000);
 
-const describeStr = `Googling ${Config.searchString}`;
-const firstItDescription = 'Each google result contains text on the first page';
-const secondItDescription = 'Each google result cointains text on the second page';
-const thirdItDescription = `Results amount greater than ${Config.expectedResults}`;
-
-describe(describeStr, function () {
-    logger.info(`${describeStr} starts`);
-
-    it(firstItDescription, async function () {
-        logger.info(`${firstItDescription} starts`);
-        for (text of await resultsPage.getSearchItemsTexts()) {
-            await expect(text.toLowerCase()).toContain(Config.searchString.toLowerCase());
-        }
-        await resultsPage.nextPage();
-        logger.info(`${firstItDescription} finished`);
+describe('describeStr', function () {
+    logger.debug('inside describe');
+    it('какать', function () {
+        logger.debug('kaban');
+        expect(true).toBe(true);
     }, 20000);
-
-    it(secondItDescription, async function () {
-        logger.info(`${secondItDescription} starts`);
-        for (text of await resultsPage.getSearchItemsTexts()) {
-            await expect(text.toLowerCase()).toContain(Config.searchString.toLowerCase());
-        }
-        logger.info(`${secondItDescription} finished`);
-    }, 20000);
-
-    it(thirdItDescription, async function() {
-        logger.info(`${thirdItDescription} starts`);
-        resultsCount = await resultsPage.getResultsCount();
-        await expect(resultsCount).toBeGreaterThan(Config.expectedResults);
-        logger.info(`${thirdItDescription} finished`);
-    }, 20000);
-
-    logger.info(`${describeStr} finished`);
 });
  
