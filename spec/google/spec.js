9fd8e59e106069e85fc871d402afa9de9d07a7a0
Dimon Mobile <mobik.dimka@gmail.com>
2019-12-16 22:27:30 +0300

    Raw working version

--- spec/google/spec.js
+++ spec/google/spec.js
@@ -1,59 +1,50 @@
 // logger configuration
-const log4js = require('log4js');
+// const log4js = require('log4js');
 
-log4js.configure({
-    appenders: {
-        file: {type: 'file', filename: 'logs/log.txt', pattern: 'yyyy-MM-dd-hh', compress: true},
-        stdout: {type: 'stdout'}
-    },
-    categories: {
-        default: { appenders: ['file', 'stdout'], level: 'debug'},
-        trc: {appenders: ['file'], level: 'trace'}
-    }
-});
+// log4js.configure({
+//     appenders: {
+//         file: {type: 'file', filename: 'logs/log.txt', pattern: 'yyyy-MM-dd-hh', compress: true},
+//         stdout: {type: 'stdout'}
+//     },
+//     categories: {
+//         default: { appenders: ['file', 'stdout'], level: 'debug'},
+//         trc: {appenders: ['file'], level: 'trace'}
+//     }
+// });
 
-const loggerDefault = log4js.getLogger();
-const loggerTrace = log4js.getLogger('trc');
+// const loggerDefault = log4js.getLogger();
+// const loggerTrace = log4js.getLogger('trc');
 
-let logger = {
-    trace: str => loggerTrace.trace(str),
-    debug: str => loggerDefault.debug(str),
-    info: str => loggerDefault.info(str),
-    warn: str => loggerDefault.warn(str),
-    error: str => loggerDefault.error(str),
-    fatal: str => loggerDefault.fatal(str)
-}
-
-// preparing webdriver
-//let browser = require('wdio').getBrowser('UiAutomator2');
-require('wdio');
-const { Builder, By, Key, until } = require('selenium-webdriver');
-//let driver = new Builder().forBrowser('UiAutomator2').build();
-let resultsCount = 0;
-let dateStarted;
-
-// preparing allure
-const AllureReporter = require('jasmine-allure-reporter');
-jasmine.getEnv().addReporter(new AllureReporter({
-    resultsDir: 'allure-results'
-}));
-
-// preparing PageObject pattern
-
-beforeAll(function () {
-    logger.info('before all');
-}, 20000);
-
-afterAll(function () {
-    logger.info('after all');
-    log4js.shutdown();
-}, 20000);
+// let logger = {
+//     trace: str => loggerTrace.trace(str),
+//     debug: str => loggerDefault.debug(str),
+//     info: str => loggerDefault.info(str),
+//     warn: str => loggerDefault.warn(str),
+//     error: str => loggerDefault.error(str),
+//     fatal: str => loggerDefault.fatal(str)
+// }
 
 describe('describeStr', function () {
-    logger.debug('inside describe');
     it('какать', function () {
-        logger.debug('kaban');
-        expect(true).toBe(true);
-    }, 20000);
+        let loginBtn = $('android=new UiSelector().description("Login Button")');
+        loginBtn.waitForExist(10000);
+        loginBtn.click();
+        let userNameTextEdit = $('android=new UiSelector().resourceId("user_username")');
+        userNameTextEdit.waitForExist(10000);
+        let userPasswordTextEdit = $('android=new UiSelector().resourceId("user_password")');
+        userPasswordTextEdit.waitForExist(10000);
+        let submitBtn = $('android=new UiSelector().resourceId("submit")');
+        submitBtn.waitForExist(10000);
+
+        userNameTextEdit.setValue('kek');
+        userPasswordTextEdit.setValue('kek');
+        submitBtn.click();
+
+        let errorMsgElement = $('android=new UiSelector().className("android.view.View").childSelector(textContains("error!"))');
+        errorMsgElement.waitForExist(10000);
+        let errorText = errorMsgElement.getText();
+        if (errorText.includes("We didn't recognize the username or password you entered. Please try again.")) {
+            console.log("UIIIIIIIIIIIIIIIIIIIIII**************************");
+        }
+    }, 50000);
 });
- 
